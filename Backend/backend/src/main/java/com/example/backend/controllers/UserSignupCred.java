1dd92694fa3646b240efb06b6c675d2df0b96abc
valente alvarez <valvarez@basecampcodingacademy.org>
2018-04-10 09:14:34 -0500

    In the UsersignupCred, deleted one of the parameters from the constructor.

--- Backend/backend/src/main/java/com/example/backend/controllers/UserSignupCred.java
+++ Backend/backend/src/main/java/com/example/backend/controllers/UserSignupCred.java
@@ -8,12 +8,11 @@ public class UserSignupCred {
     public String city;
     public String county;
     public String home_address;
-    public Integer days;
     public String username;
     public String password_hash;
     public String sessionKey;
 
-    public UserSignupCred(Integer id, String first_name, String last_name, String email, String city, String county, String home_address, Integer days,
+    public UserSignupCred(Integer id, String first_name, String last_name, String email, String city, String county, String home_address,
                           String username, String password_hash, String sessionKey){
         this.id = id;
         this.first_name = first_name;
@@ -22,7 +21,6 @@ public class UserSignupCred {
         this.city = city;
         this.county = county;
         this.home_address = home_address;
-        this.days = days;
         this.username = username;
         this.password_hash = password_hash;
         this.sessionKey = sessionKey;
